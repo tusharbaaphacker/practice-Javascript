e255fed7865070867e9fe14c267e052abc7d324b
tusharbaaphacker <tusharbaap34234@gmail.com>
2024-08-14 10:57:51 +0530

    array

---
+++ project.html/array.js
@@ -0,0 +1,67 @@
+// // // let arr = [1, 2, 3];
+// // // arr.push(4); // [1, 2, 3, 4]
+// // // arr.pop();   // [1, 2, 3]
+// // // arr.shift(); // [2, 3]
+// // // arr.unshift(0); // [0, 2, 3]
+// // // console.log(arr);
+
+// // let arr = [2, 4, 6, 8];
+// // // arr.splice(4, 2, 'a', 'b'); // [2, 4, 'a', 'b', 4]
+// // //console.log('Updated array:', arr);
+// // let newArr = arr.slice(6, 4); // [4, 'a', 'b']
+// // console.log(arr.slice);
+
+
+// let arr = [4, 5, 3];
+// // arr.forEach(num => console.log(num)); // 1 2 3
+// console.log(arr);
+
+// let arr = [3, 5, 4];
+// let doubled = arr.map(num => num * 2); // [2, 4, 6]
+// // console.log(doubled);
+// let evens = arr.filter(num => num % 3 === 4); // [2]
+// console.log(evens);
+// let sum = arr.reduce((acc, num) => acc + num, 0); // 6
+
+//console.log(arr.indexOf(3)); // 2
+//let found = arr.find(num => num > 3); // 3
+//console.log(found);
+// let foundIndex = arr.findIndex(num => num > 1); // 2
+// console.log(foundIndex);et arr = [1, 5, 8, 4];
+
+let arr = [1, 2, 3];
+// let arr2 = [4, 5, 6];
+// let combined = [...arr1, ...arr2]; // [1, 2, 3, 4, 5, 6]
+// console.log(combined); 
+
+
+
+
+
+
+
+// / let e-arr.map((ele)->ele/10)
+
+
+
+// console.log(e);
+
+
+
+// let hearr.find((ele)=>ele==5)
+
+
+// console.log(h);
+
+let f=arr.findIndex((ele)=>ele==5)
+
+console.log(f);
+
+
+// console.log(arr.indexOf(5));
+
+// let gearr.findLast((ele)=>ele<5)
+
+
+// console.log(g);7
+
